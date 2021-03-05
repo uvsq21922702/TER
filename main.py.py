7b321bf06e9c0c5b60b02595ee2a1687912133da
uvsq21922702 <yacine.yousfi@ens.uvsq.fr>
2021-03-05 22:11:26 +0100

    initial commit

---
+++ main.py.py
@@ -0,0 +1,24 @@
+import os
+import requests
+import schedule
+import time
+
+index = {'count': 0}
+
+
+def func(index):
+    url = 'https://opendata.saemes.fr/explore/dataset/places-disponibles-parkings-saemes/download/?format=csv&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true&csv_separator=%3B'
+    r = requests.get(url)
+
+    with open(os.path.join(r"file%s.csv" % index['count']), 'wb') as f:
+        f.write(r.content)
+        print("File downloaded")
+
+    indexdcvu['count'] += 1
+
+
+schedule.every(3).minutes.do(func, index)
+
+while True:
+    schedule.run_pending()
+    time.sleep(2)
